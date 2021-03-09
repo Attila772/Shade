51ad63583e0fba7e740150d2cff4d3f43379d6ae
Ferekt <78617056+Ferekt@users.noreply.github.com>
2021-03-09 11:51:50 +0100

    pathfinding updated

--- Player/Guard.gd
+++ Player/Guard.gd
@@ -5,7 +5,7 @@ var speed = 125
 var animation
 var anim_mode="Walk"
 var angle
-var move_direction = "N"
+var move_direction = "S"
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
@@ -17,7 +17,6 @@ func _ready():
 
 func movementloop():
 	movement = $'../../Navigation2D/Line2D'.points[1]-self.position
-	movement=movement.normalized()*speed
 	angle = rad2deg(Vector2(0,-1).angle_to(movement))
 	if (angle>-22.5 && angle<22.5):
 		move_direction="N"
@@ -27,7 +26,7 @@ func movementloop():
 		move_direction="E"
 	if (angle>112.5 && angle<157.5):
 		move_direction="SE"
-	if (angle>157.5 && angle<-157.5):
+	if (angle>157.5 || angle<-157.5):
 		move_direction="S"
 	if (angle>-157.5 && angle<-112.5):
 		move_direction="SW"
@@ -35,11 +34,11 @@ func movementloop():
 		move_direction="W"
 	if (angle>-67.5 && angle<-22.5):
 		move_direction="NW"
+	movement=movement.normalized()*speed
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
 	movementloop()
 	move_and_slide(movement)
-	Animationloop()
 	
 func _process(delta):
 	Animationloop()
