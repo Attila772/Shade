6fb69baaca54ef00a83c946782750f32f21b7aa6
Ferekt <78617056+Ferekt@users.noreply.github.com>
2021-03-17 18:27:37 +0100

    Update FOW.gd

--- FOW/FOW.gd
+++ FOW/FOW.gd
@@ -2,12 +2,14 @@ extends Node2D
 var parent_body
 var center = Vector2(0,0)
 var radius = 500
-var color = Color(1.0, 0.0, 0.0,0.2)
+var color = Color(0.0, 1.0, 0.0,0.2)
 var angle_from = 0 
 var angle_to = 90
 var movement = Vector2(1,1)
 var breee = true
 var got_track = false
+var spot_time = 1
+var delta_for_timer
 enum {
 	Patrol
 	Chase
@@ -47,15 +49,20 @@ func draw_circle_arc_poly(center, radius ,color):
 		points_arc.push_back(angle_point)
 		draw_line(points_arc[i-1],points_arc[i],Color(0,0,1,1)) 
 		if raycast.is_colliding() && raycast.get_collider().name == "Player":
+			spot_time -= delta_for_timer
 			seen = true
 	draw_polygon(points_arc, colors)
-	if seen :
+	if !seen : 
+		spot_time = 1
+		color = Color(0.0, 1.0, 0.0,0.2)
+	if seen && spot_time<0 :
 		parent_body.Line.state= Chase
 		got_track= true
+		color = Color(1.0, 0.0, 0.0,0.2)
 	if got_track && !seen :
 		parent_body.Line.state = Search
 		got_track=false
-	seen= false
+		
 func _draw():
    draw_circle_arc_poly(center, radius, color )
 
@@ -66,6 +73,7 @@ func Organise_RayCasts():
 		raycast.cast_to = (movement.normalized().rotated(deg2rad((i-15)*3))*radius)
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	delta_for_timer=delta
 	position = parent_body.position
 	movement = parent_body.dir
 	Organise_RayCasts()
