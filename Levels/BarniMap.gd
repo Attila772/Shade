10b4e6f7cbcb86ab242fd38b1b953ca75c6c2e4c
Barnibarna329 <79806417+Barnibarna329@users.noreply.github.com>
2021-05-16 14:22:28 +0200

    BarniMap1.2

--- Levels/BarniMap.gd
+++ Levels/BarniMap.gd
@@ -1,5 +1,8 @@
 extends Node2D
 
+var guardlvl=2
+var Guard1=[guardlvl,2,Vector2(924,360),Vector2(2693,1508),Vector2(3147,1145),Vector2(1283,157),Vector2(3147,1145),Vector2(2693,1508),Vector2(2500,1164),Vector2(1333,598),Vector2(401,579)]
+var Camera1=[-180,-90,0]
 
 # Declare member variables here. Examples:
 # var a = 2
