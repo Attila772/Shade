23fc5f364c05e8955b445c16fd282647d0ede003
Barnibarna329 <79806417+Barnibarna329@users.noreply.github.com>
2021-05-18 18:08:35 +0200

    PerfectSoundSettings

--- MainMenu.gd
+++ MainMenu.gd
@@ -1,5 +1,5 @@
 extends Node2D
-
+var soundlvl=Global.SoundLevel*0.6-60
 
 # Declare member variables here. Examples:
 # var a = 2
@@ -9,7 +9,7 @@ extends Node2D
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	var masterBus=AudioServer.get_bus_index("Master")
-	AudioServer.set_bus_volume_db(masterBus,-24)
+	AudioServer.set_bus_volume_db(masterBus,soundlvl)
 	pass
 func _process(delta):
 	if !$Ambient.playing:
